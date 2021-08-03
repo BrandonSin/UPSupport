5fd611a960e8cdb17ab67a01cc954e4e05f7b515
Brandon Sin <46876645+BrandonSin@users.noreply.github.com>
2021-08-03 11:54:19 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
-import UpLogo from '../images/AppLogoTransparent';
+import UpLogo from '../images/AppLogoTransparent.png';
 import './Navbar.css';
 
 function Navbar() {
